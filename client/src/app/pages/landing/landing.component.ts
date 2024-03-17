3506cfa772576d725fab9044be9949d8b300927e
obiwan007 <markus.miertschink@hotmail.com>
2024-03-17 08:33:33 +0100

    Landingpage with multiple Videos

--- client/src/app/pages/landing/landing.component.ts
+++ client/src/app/pages/landing/landing.component.ts
@@ -11,6 +11,20 @@ import { Router, RouterModule } from '@angular/router';
 })
 export class LandingComponent {
   router = inject(Router);
+
+  videos = [
+    'https://firebasestorage.googleapis.com/v0/b/decentprofile-95df0.appspot.com/o/My%20Movie%201.mp4?alt=media&token=f8cfd1ca-6a22-48ab-aaf7-c4e8f4ae336c',
+    'https://firebasestorage.googleapis.com/v0/b/decentprofile-95df0.appspot.com/o/IMG_0145.mp4?alt=media&token=fc5d6ce8-8e62-4bfe-8ef3-be8a89a1ba00'];
+  currentVideo: string;
+
+  /**
+   *
+   */
+  constructor() {
+    const v = Math.min(this.videos.length - 1, Math.round(Math.random() * (this.videos.length - 1)));
+    this.currentVideo = this.videos[v];
+  }
+
   login() {
     this.router.navigateByUrl('/login');
   }
