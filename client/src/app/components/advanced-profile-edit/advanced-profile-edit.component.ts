6114e6661768882f245aaab5a8150f7bbfa0541c
randomcoffeesnob <ydietler@gmail.com>
2024-03-30 00:35:35 +0100

    Added first fields

--- client/src/app/components/advanced-profile-edit/advanced-profile-edit.component.ts
+++ client/src/app/components/advanced-profile-edit/advanced-profile-edit.component.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BasicProfileEditComponent } from '../basic-profile-edit/basic-profile-edit.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { EditModeDirective } from '../editable/edit-mode.directive';
+import { EditableComponent } from '../editable/editable.component';
+import { FocusableDirective } from '../editable/focusable.directive';
+import { ViewModeDirective } from '../editable/view-mode.directive';
+import { ProfileChartComponent } from '../profile-chart/profile-chart.component';
+import { ProfileDetailsComponent } from '../profile-details/profile-details.component';
+import { RenderStepComponent } from '../render-step/render-step.component';
+import { RenderStepsComponent } from '../render-steps/render-steps.component';
+import { Profile, ProfileType, PumpMode, SensorType, Step, TransitionMode } from '../../models/profile';
+import { MatSelect, MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-advanced-profile-edit',
   standalone: true,
-  imports: [],
+  imports: [ProfileChartComponent,
+    MatCardModule,
+    RenderStepComponent,
+    ProfileDetailsComponent, RenderStepComponent, RenderStepsComponent,
+    AdvancedProfileEditComponent, BasicProfileEditComponent, MatButtonModule, MatSliderModule, CommonModule, FormsModule,
+    ReactiveFormsModule, MatInputModule,
+    EditableComponent,
+    ViewModeDirective,
+    EditModeDirective,
+    FocusableDirective,
+    MatToolbarModule,
+    MatIconModule,
+    MatSelectModule,
+  ],
+
   templateUrl: './advanced-profile-edit.component.html',
   styleUrl: './advanced-profile-edit.component.css'
 })
-export class AdvancedProfileEditComponent extends BasicProfileEditComponent {
-
+export class AdvancedProfileEditComponent extends BasicProfileEditComponent  {
+  pumpMode = PumpMode;
+  transitionMode = TransitionMode;
 }
