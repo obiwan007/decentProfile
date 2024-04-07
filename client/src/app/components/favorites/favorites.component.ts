a16db4a1ceca2188e704244f18df98794bb2a2ef
obiwan007 <markus.miertschink@hotmail.com>
2024-04-07 10:19:10 +0200

    Show list of favorites

--- client/src/app/components/favorites/favorites.component.ts
+++ client/src/app/components/favorites/favorites.component.ts
@@ -1,22 +1,30 @@
-import { AsyncPipe, CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule, JsonPipe } from '@angular/common';
 import { Component, OnDestroy } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSelectChange, MatSelectModule } from '@angular/material/select';
 import { FavoritesService } from '../../services/favorites.service';
 import { Observable, Subscription } from 'rxjs';
 import { Favorite } from '../../models/favorite';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { RenderStepComponent } from '../render-step/render-step.component';
 
 @Component({
   selector: 'app-favorites',
   standalone: true,
-  imports: [MatSelectModule,
+  imports: [
+    MatSelectModule,
     MatFormFieldModule,
+    MatListModule,
     FormsModule,
     MatIconModule,
+    MatLabel,
+    MatButtonModule, MatIconModule,
     CommonModule,
     AsyncPipe,
+    JsonPipe,
   ],
   templateUrl: './favorites.component.html',
   styleUrl: './favorites.component.scss'
@@ -28,7 +36,10 @@ export class FavoritesComponent implements OnDestroy {
 
   constructor(private _favoritesSrv: FavoritesService) {
     this.favorites$ = _favoritesSrv.favorites();
-    this.subs.push(this.favorites$.subscribe(f => this.favorites = f));
+    this.subs.push(this.favorites$.subscribe(f => {
+      console.log("Favlist:", f);
+      this.favorites = f
+    }));
   }
   ngOnDestroy() { this.subs.forEach(s => s.unsubscribe()); }
 
